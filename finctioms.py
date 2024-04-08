6b8f44e01cd39f1fe06c8b945c0922b59949ed7b
KRVishnuChaithanya <161932583+KRVishnuChaithanya@users.noreply.github.com>
2024-04-08 10:22:59 +0530

    Add files via upload

---
+++ finctioms.py
@@ -0,0 +1,15 @@
+a="12345"
+print(a.isupper())
+print(a.islower())
+print(a.isalpha())
+print(a.isalnum())
+print(a.isdigit())
+print(a.isspace())
+print(a.istitle())
+print(a.startswith('4'))
+print(a.endswith('6'))
+print(a.find("5"))
+print(len(a))
+print(min(a))
+print(max(a))
+
