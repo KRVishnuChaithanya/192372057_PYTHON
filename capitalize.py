a00066d8ce737e92b7eeb9c89ac975860d6c495e
KRVishnuChaithanya <161932583+KRVishnuChaithanya@users.noreply.github.com>
2024-04-08 10:29:18 +0530

    Add files via upload

---
+++ capitalize.py
@@ -0,0 +1,14 @@
+a="good morning"
+print(a.capitalize())
+print(a.upper())
+print(a.lower())
+print(a.title())
+print(a.swapcase())
+print(a.split("good "))
+print(a.center(19,"*"))
+print(a.count('o'))
+print(a.replace('good',' morning'))
+b="happy"
+a="-"
+print(a.join(b))
+
