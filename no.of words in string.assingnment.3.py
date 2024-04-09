5949c70f212e6a32910400ca4c1bbd6c92dcc916
KRVishnuChaithanya <161932583+KRVishnuChaithanya@users.noreply.github.com>
2024-04-09 09:50:25 +0530

    Add files via upload

---
+++ no.of words in string.assingnment.3.py
@@ -0,0 +1,8 @@
+countOfWords = len("i love programming".split())
+print("Count of Words in the given Sentence:", countOfWords)
+
+
+print(len("python is my fav language".split()))
+
+
+print(len(input("Enter Input:").split()))
