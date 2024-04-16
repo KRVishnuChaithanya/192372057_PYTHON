0d63eb5931d7b5e95a8bc4534f545e082e8893d6
KRVishnuChaithanya <161932583+KRVishnuChaithanya@users.noreply.github.com>
2024-04-16 08:35:51 +0530

    Add files via upload

--- list of parameters1.py
+++ list of parameters1.py
@@ -4,12 +4,12 @@ a=[1,2,3,4,5]
 remove(a)
 print(a)
 def inside(a):
- for i in range(0,len(a),1):
-     a[i]=a[i]+10
-     print("inside",a)
+    for i in range(0,len(a),1):
+        a[i]=a[i]+10
+        print("inside",a)
 a=[1,2,3,4,5]
 inside(a)
-print("outside",a)  
+print("outside",a)
 def insert(a):
     a.insert(0,30)
 a=[1,2,3,4,5]
